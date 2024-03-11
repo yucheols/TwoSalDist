a5e42f83a0f7adcf017f106428c3acd7c60ce094
Yucheol Shin <brongersmai2@gmail.com>
2024-03-12 00:19:21 +0900

    commit

--- codes/getEnvData.R
+++ codes/getEnvData.R
@@ -6,4 +6,4 @@ library(terra)
 ext <- c(124.1824, 130.9404, 33.11208, 43.00605)
 
 # get data
-paleoclim(period = c('cur'), resolution = '30s', region = ext, as = 'raster', cache_path = 'data/CHELSA')
+pc_data <- paleoclim(period = c('cur'), resolution = '30s', region = ext, as = 'raster', cache_path = 'data/CHELSA')
