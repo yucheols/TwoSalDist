1ba7e5a4c484ab020edd41018b3e488b83155ea5
Yucheol Shin <brongersmai2@gmail.com>
2024-03-11 17:57:48 +0900

    update

--- codes/model_CHELSA.R
+++ codes/model_CHELSA.R
@@ -1,1 +1,11 @@
-##### modeling with CHELSA data 
+##### Current ENM with CHELSA data 
+# clean up working env
+rm(list = ls(all.names = T))
+
+# set seed 
+set.seed(321)
+
+# prevent encoding error
+
+
+# load packages
