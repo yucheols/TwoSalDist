093bc1fbb15c16f88c99460555556ba301c90938
Yucheol Shin <brongersmai2@gmail.com>
2024-03-25 17:06:40 +0900

    commit

--- codes/hindcasting.R
+++ codes/hindcasting.R
@@ -7,3 +7,81 @@ gc()
 library(raster)
 library(dplyr)
 library(ENMeval)
+
+##### Part 14 ::: hindcasting run --------------------------------------------------------------------------------------------------------------------
+
+### load hindcast layers
+# mid-Pliocene Warm Period (mPWP)
+mpwp <- raster::stack(list.files(path = 'data/hindcast_layers/processed/mPWP', pattern = '.bil$', full.names = T))
+print(mpwp)
+plot(mpwp[[1]])
+
+# Marine Isotope Stage 19 (MIS19)
+mis <- raster::stack(list.files(path = 'data/hindcast_layers/processed/MIS19', pattern = '.bil$', full.names = T))
+print(mis)
+plot(mis[[1]])
+
+# Last Interglacial (LIG)
+lig <- raster::stack(list.files(path = 'data/hindcast_layers/processed/LIG', pattern = '.bil$', full.names = T))
+print(lig)
+plot(lig[[1]])
+
+# Last Glacial Maximum (LGM)
+lgm <- raster::stack(list.files(path = 'data/hindcast_layers/processed/LGM', pattern = '.bil$', full.names = T))
+print(lgm)
+plot(lgm[[1]])
+
+# Mid-Holocene (MH)
+mh <- raster::stack(list.files(path = 'data/hindcast_layers/processed/MH', pattern = '.bil$', full.names = T))
+print(mh)
+plot(mh[[1]])
+
+
+### load climate-only models
+# O. koreanus 
+o.models_clim <- readRDS('tuning_experiments/output_model_rds/O_koreanus_clim_only_WorldClim.rds')
+print(o.models_clim)
+
+k.models_clim <- readRDS('tuning_experiments/output_model_rds/K_koreana_clim_only_WorldClim.rds')
+print(k.models_clim)
+
+
+##### model prediction to past climatic conditions
+# make function for prediction
+model_predictr <- function(model, preds.list, pred.names) {
+  require(dismo)
+  
+  output <- list()
+  
+  for (i in 1:length(preds.list)) {
+    make.pred <- dismo::predict(object = model, x = preds.list[[i]], progress = 'text')
+    output[[i]] <- make.pred
+  }
+  output.pred <- raster::stack(output)
+  names(output.pred) = pred.names
+  return(output.pred)
+}
+
+### O.koreanus
+# run
+o.hinds <- model_predictr(model = o.models_clim$models[[1]], 
+                          preds.list = list(mpwp, mis, lig, lgm, mh), 
+                          pred.names = c('mPWP', 'MIS19', 'LIG', 'LGM', 'MH'))
+
+# print & plot
+print(o.hinds)
+plot(o.hinds)
+
+
+### K.koreana
+# run
+k.hinds <- model_predictr(model = k.models_clim$models[[1]],
+                          preds.list = list(mpwp, mis, lig, lgm, mh),
+                          pred.names = c('mPWP', 'MIS19', 'LIG', 'LGM', 'MH'))
+
+# print & plot
+print(k.hinds)
+plot(k.hinds)
+
+
+##### Part 14 ::: MESS --------------------------------------------------------------------------------------------------------------------
