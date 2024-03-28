ff408fb86c18d33063b01cece9c77506f86cb4a7
Yucheol Shin <brongersmai2@gmail.com>
2024-03-29 05:01:32 +0900

    commit

--- codes/clim_only_model_CHELSA.R
+++ codes/clim_only_model_CHELSA.R
@@ -21,3 +21,131 @@ bg <- read.csv('data/bg/set1/bg1_10000.csv') %>% select('long', 'lat')
 head(bg)
 
 ## load envs
+envs <- raster::stack(list.files(path = 'data/masked/CHELSA', pattern = '.bil$', full.names = T))
+envs <- raster::stack(subset(envs, c('bio1', 'bio4', 'bio12', 'bio13', 'bio14', 'bio15')))
+print(envs)
+plot(envs[[1]])
+
+
+#### Model tuning
+# automate model tuning 
+# type 1 == minimum or.10p.avg as primary criterion // type 2 == delta.AICc <= 2 as primary criterion 
+test_models <- function(taxon.name, occs, envs, bg.list, tune.args, partitions, partition.settings = NULL, user.grp = NULL, type) {
+  output <- list()
+  models <- list()
+  preds <- list()
+  contrib <- list()
+  
+  if (type == 'type1') {
+    for (i in 1:length(bg.list)) {
+      
+      # make models
+      eval <- ENMeval::ENMevaluate(taxon.name = taxon.name, occs = occs, envs = envs, bg = bg.list[[i]], 
+                                   tune.args = tune.args, partitions = partitions, partition.settings = partition.settings, 
+                                   user.grp = user.grp[[i]], doClamp = T, algorithm = 'maxent.jar', parallel = T, parallelType = 'doSNOW')
+      
+      # get results
+      eval.res <- ENMeval::eval.results(eval)
+      
+      # get optimal parameter combinations
+      opt.param <- eval.res %>% dplyr::filter(or.10p.avg == min(or.10p.avg)) %>%
+        dplyr::filter(auc.diff.avg == min(auc.diff.avg)) %>%
+        dplyr::filter(auc.val.avg == max(auc.val.avg))
+      
+      output[[i]] <- opt.param
+      metrics <- dplyr::bind_rows(output)
+      
+      # get optimal model per iteration
+      opt.model <- ENMeval::eval.models(eval)[[opt.param$tune.args]]
+      models[[i]] <- opt.model
+      
+      # get variable importance for each best model
+      var.imp <- ENMeval::eval.variable.importance(eval)[[opt.param$tune.args]]
+      contrib[[i]] <- var.imp
+      
+      # get optimal predictions per iteration
+      opt.pred <- ENMeval::eval.predictions(eval)[[opt.param$tune.args]]
+      preds[[i]] <- opt.pred
+      preds.stack <- raster::stack(preds)
+    }
+  }
+  else if (type == 'type2') {
+    for (i in 1:length(bg.list)) {
+      
+      # make models
+      eval <- ENMeval::ENMevaluate(taxon.name = taxon.name, occs = occs, envs = envs, bg = bg.list[[i]], 
+                                   tune.args = tune.args, partitions = partitions, partition.settings = partition.settings,
+                                   user.grp = user.grp[[i]], doClamp = T, algorithm = 'maxent.jar', parallel = T, parallelType = 'doSNOW')
+      
+      # get results
+      eval.res <- ENMeval::eval.results(eval)
+      
+      # get optimal parameter combinations
+      opt.param <- eval.res %>% dplyr::filter(delta.AICc <= 2) %>%
+        dplyr::filter(or.10p.avg == min(or.10p.avg)) %>%
+        dplyr::filter(auc.val.avg == max(auc.val.avg))
+      
+      output[[i]] <- opt.param
+      metrics <- dplyr::bind_rows(output)
+      
+      # get optimal model per iteration
+      opt.model <- ENMeval::eval.models(eval)[[opt.param$tune.args]]
+      models[[i]] <- opt.model
+      
+      # get variable importance for each best model
+      var.imp <- ENMeval::eval.variable.importance(eval)[[opt.param$tune.args]]
+      contrib[[i]] <- var.imp
+      
+      # get optimal predictions per iteration
+      opt.pred <- ENMeval::eval.predictions(eval)[[opt.param$tune.args]]
+      preds[[i]] <- opt.pred
+      preds.stack <- raster::stack(preds)
+    }
+  }
+  return(list(metrics = metrics, models = models, preds = preds.stack, contrib = contrib))
+}
+
+
+### tuning arguments
+tune.args <- list(fc = c('L', 'Q', 'H', 'P', 'LQ', 'LP', 'QH', 'QP', 'HP', 'LQH', 'LQP', 'LQHP', 'LQHPT'), 
+                  rm = seq(0.5,5, by = 0.5))
+
+
+### O.koreanus == 
+# run
+o.models_clim <- test_models(taxon.name = 'O.koreanus', occs = o.occs, envs = envs, bg.list = list(bg), tune.args = tune.args, 
+                             partitions = 'checkerboard2', partition.settings = list(aggregation.factor = c(7,7)), type = 'type1')
+
+# look at metric
+
+# look at variable importance
+
+# look at prediction
+
+# save models
+
+# export contribution
+
+# export metric
+
+# export continuous pred
+
+
+### K.koreana
+# run
+k.models_clim <- test_models(taxon.name = 'K.koreana', occs = k.occs, envs = envs, bg.list = list(bg), tune.args = tune.args, 
+                             partitions = 'checkerboard2', partition.settings = list(aggregation.factor = c(7,7)), type = 'type1')
+
+# look at metric
+
+# look at variable importance
+
+# look at prediction
+
+# save models
+
+# export contribution
+
+# export metric
+
+# export continuous pred
