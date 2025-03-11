1be7934eec1d6437f5843ed1edb98adadca970e4
Yucheol Shin <brongersmai2@gmail.com>
2025-03-11 09:21:13 -0400

    organize

--- codes/getEnvData.R
+++
@@ -1,9 +0,0 @@
-#####  get PaleoClim data & CHELSA data using the package rpaleoclim //// WorldClim data are downloaded directly from the website
-library(rpaleoclim)
-library(terra)
-
-#####  get CHELSA current data == 1979 - 2013
-ext <- c(124.1824, 130.9404, 33.11208, 43.00605)
-
-# get data
-pc_data <- paleoclim(period = c('cur'), resolution = '30s', region = ext, as = 'raster', cache_path = 'data/CHELSA')
